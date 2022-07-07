102a49c7e9d4dbfdc8cb1f5b8b79641d57a33628
deniz doğru <denizdogru@gmail.com>
2022-07-07 14:41:59 +0300

    other operations not working  orrectly

--- app/Calculator.cs
+++ app/Calculator.cs
@@ -1,5 +1,5 @@
-// Simple calculator program using switch statement
-// First commit
+// Simple calculator program using lists and less error prone
+
 
 using System;
 
@@ -9,13 +9,13 @@ namespace calculatorApp
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("CALC APP");
-            Console.WriteLine("---------------");
+          
+            Console.WriteLine("----------CALC APP---------");
             Console.WriteLine("1. Addition");
             Console.WriteLine("2. Substraction");
             Console.WriteLine("3. Multiplication  ");
             Console.WriteLine("4. Division");
-            Console.WriteLine("---------------");
+            Console.WriteLine("---------------------------");
 
             Console.WriteLine("Please select an operation from the menu.");
             var action = Console.ReadLine();
@@ -36,29 +36,65 @@ namespace calculatorApp
                     List<int> numbers = new List<int>();
                     // input number(n), sum total(sum)
                     int n,sum = 0;
-                        
+
                     // This while loop waits for user input and converts that input to an integer
                     while (int.TryParse(Console.ReadLine(), out n))
                     {
+                        // here we add the entered number to the sum
                         sum += n;
+                        // and to the list to track how many we've added.
                         numbers.Add(n);
                     }
-                    Console.WriteLine("Sum: " + (numbers.Count > 0? sum  : 0));
+                    //Finally we make sure that we have more than 0 numbers and print to the console.
+                    Console.WriteLine("Sum: " + (numbers.Count > 0 ? sum : 0));
                     break;
                 }
                 if (action == "2")
                 {
                     Console.WriteLine($"Operation, {action} is selected!");
+
+                    List<int> numbers = new List<int>();
+                    int n,sum=0;
+
+                    while(int.TryParse(Console.ReadLine(), out n))
+                    {
+                        sum = n ;
+                        numbers.Add(n);
+                    }
+                    Console.WriteLine("Substraction: " +(numbers.Count > 0? sum : 0));
                     break;
                 }
                 if (action == "3")
                 {
                     Console.WriteLine($"Operation, {action} is selected!");
+                    Console.WriteLine("Enter integers to multiplicate. ");
+
+                    List<int> numbers = new List<int>();
+                    int n = 0, sum=1;
+
+                    while(int.TryParse(Console.ReadLine(), out n))
+                    {
+                        sum *= n ;
+                        numbers.Add(n);
+                    }
+                    Console.WriteLine("Multiplication: " +(numbers.Count > 0? sum : 0));
                     break;
                 }
                 if (action == "4")
                 {
                     Console.WriteLine($"Operation, {action} is selected!");
+                    Console.WriteLine("Enter integers to divide. ");
+
+                    List<int> numbers = new List<int>();
+                    int n = 0, sum=1;
+
+                    while(int.TryParse(Console.ReadLine(), out n))
+                    {
+                        
+                        numbers.Add(n);
+                        sum /= n ;
+                    }
+                    Console.WriteLine("Division: " +(numbers.Count > 0? sum : 0));
                     break;
                 }
                 else
